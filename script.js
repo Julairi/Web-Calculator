1484446b6d5eb465f645b4bce7619b41f098416f
Julairi <98028898+Julairi@users.noreply.github.com>
2022-03-17 12:32:03 +0000

    Initial, static commit
    CSS and HTML files, static.

---
+++ script.js
@@ -0,0 +1,7 @@
+//grabbing vars
+let values = document.querySelectorAll(".values");
+let operators = document.querySelectorAll(".operators");
+let output = document.querySelector(".result");
+
+
+let isPressed = false;
